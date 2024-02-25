68609d910f794d1bb1243bb5fd0571179691574f
Rohith <rohithsuresh207@gmail.com>
2024-02-25 18:06:24 +0530

    zig elf setup

--- src/elf.zig
+++ src/elf.zig
@@ -7,6 +7,9 @@ pub fn test_elf_parse() !void {
     var buffer: [4096]u8 align(8) = undefined;
     var bytesElf = try file.read(buffer[0..]);
     _ = bytesElf;
-    var elfHeader = @ptrCast(*Elf.Elf64_Ehdr, &buffer);
+    var elfHeader: *Elf.Elf64_Ehdr = @ptrCast(*Elf.Elf64_Ehdr, &buffer);
     std.debug.print("{!}\n", .{elfHeader});
+    var bufferCastToOne = @alignCast(1, &buffer);
+    var elfSectionHeader = @ptrCast(*Elf.Elf64_Shdr, &bufferCastToOne[elfHeader.e_shoff..]);
+    std.debug.print("{!}\n", .{elfSectionHeader});
 }
